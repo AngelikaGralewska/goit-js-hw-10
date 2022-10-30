5a0ee91e5a621f89775f442ea6deeaf10e220009
AngelikaGralewska <angelika.gralewska@gmail.com>
2022-10-30 18:36:25 +0100

    style

--- src/index.js
+++ src/index.js
@@ -28,7 +28,6 @@ function countriesListOk(countries) {
             Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
             list.innerHTML = '';
             info.innerHTML = '';
-            return;
         }
         if (countries.length > 1 && countries.length <= 10) {
             const countriesList = countries.map(country => countryItem(country)).join('');
@@ -43,30 +42,31 @@ function countriesListOk(countries) {
 
       function countryItem({ flags, name }) {
         return `
-        <li class="country-list_item">
-          <img " src="${flags.svg}" alt="${name.official}" width="20" />
+        <li>
+        <div class="country-list_item">
+          <img " src="${flags.svg}" alt="${name.official}" width="20" height="10" />
           <h5 >${name.official}</h5>
+          <div>
         </li>`;
-      }
+      };
       
       function countryСard({ flags, name, capital, population, languages }) {
         return `
-          <div class="country-info">
-            <div class="country-info_box">
-              <img src="${flags.svg}" alt="${name.official}" width="50" />
+            <div>
+              <img src="${flags.svg}" alt="${name.official}" width="60" height="30"/>
               <h3 >${name.official}</h3>
             </div>
             <ul>
-                <li>Capital: ${capital}</li>
-                <li>Population: ${population}</li>
-                <li>Languages: ${Object.values(languages)}</li>
+                <li><b>Capital:</b> ${capital}</li>
+                <li><b>Population:</b> ${population}</li>
+                <li><b>Languages:</b> ${Object.values(languages)}</li>
             </ul>`;
       };
 
 function countriesListError(){
-    Notiflix.Notify.failure('Oops, there is no country with that name');
-    list.innerHTML = '';
-    info.innerHTML = '';
+   Notiflix.Notify.failure('Oops, there is no country with that name');
+    //list.innerHTML = '';
+    //info.innerHTML = '';
     return;
 };
 
